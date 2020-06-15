c7982a9ea5c9a4aa2d3f7279d0a8f3b096b32bd0
Pras52 <Prashantiwari52@gmail.com>
2020-06-15 12:12:17 +0530

    EmpService Done

--- EMS/src/main/java/com/prashant/EmpService/EmpService.java
+++ EMS/src/main/java/com/prashant/EmpService/EmpService.java
@@ -1,5 +1,32 @@
 package com.prashant.EmpService;
 
 public class EmpService {
+	
+	
+	private String Name;
+	private int age;
+	
+	public EmpService(){}
+	public EmpService(String name , int age)
+	{
+		super();
+		this.setAge(age);
+		this.setName(name);
+		
+	}
+	
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
 
 }
